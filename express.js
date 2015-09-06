fc359d4c3ca752a015234adee22c0bdda73a104b
Byrdann Fox <mobileupstate@gmail.com>
2015-09-06 15:24:15 -0400

    finished implementing the routes for the REST API

--- express.js
+++ express.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(logger('dev'));
 
-var db = mongoskin.db('@localhost:27017/test', {
+var db = mongoskin.db('@localhost:27017/loopi', {
   safe: true
 });
 
@@ -22,4 +22,45 @@ app.param('collectionName', function (req, res, next, collectionName) {
 
 app.get('/', function (req, res, next) {
   res.send('Please, select a collection, e.g., /collections/messages');
+});
+
+// refactor the file structure for this app
+
+app.post('/collections/:collectionName', function (req, res, next) {
+  req.collection.insert(req.body, {}, function (e, results) {
+    if (e) return next(e);
+    res.send(results);
+  });
+});
+
+app.get('/collections/:collectionName', function (req, res, next) {
+  req.collection.find({}, { limit: 10, sort: { '_id': -1 }).toArray(function (e, results) {
+    if (e) return next(e);
+    res.send(results);
+  });
+});
+
+app.get('/collections/:collectionName/:id', function (req, res, next) {
+  req.collection.findById(req.params.id, function (e, result) {
+    if (e) return next(e);
+    res.send(result);
+  });
+});
+
+app.put('/collections/:collectionName/:id', function (req, res, next) {
+  req.collection.updateById(req.params.id, { $set: req.body }, { safe: true, multi: false }, function (e, result) {
+    if (e) return next(e);
+    res.send((result === 1) ? { msg: 'success' } : { msg: 'error' });
+  });
+});
+
+app.delete('/collections/:collectionName/:id', function (req, res, next) {
+  req.collection.removeById(req.params.id, function (e, result) {
+    if (e) return next(e);
+    res.send((result === 1) ? { msg: 'success' } : { msg: 'error' });
+  });
+});
+
+app.listen(3000, function () {
+  console.log('Express server listening on port 3000');
 });

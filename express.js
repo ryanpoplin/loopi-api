b18e523eec7e594244c8b6ff7b95708e0da09533
Byrdann Fox <mobileupstate@gmail.com>
2015-09-06 15:26:11 -0400

    fixed syntax error in express.js

--- express.js
+++ express.js
@@ -34,7 +34,7 @@ app.post('/collections/:collectionName', function (req, res, next) {
 });
 
 app.get('/collections/:collectionName', function (req, res, next) {
-  req.collection.find({}, { limit: 10, sort: { '_id': -1 }).toArray(function (e, results) {
+  req.collection.find({}, { limit: 10, sort: { '_id': -1 } }).toArray(function (e, results) {
     if (e) return next(e);
     res.send(results);
   });
